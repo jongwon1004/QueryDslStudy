90534143a7343fe61d4e4fd80b08cbd5c047297a
jongwon1004 <whddnjs3340@naver.com>
2024-01-15 00:19:27 +0900

    commit

---
+++ src/test/java/study/querydsl/QueryDslBasicTest.java
@@ -0,0 +1,266 @@
+package study.querydsl;
+
+import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QTeam;
+import study.querydsl.entity.Team;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.*;
+
+@SpringBootTest
+@Transactional
+public class QueryDslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    JPAQueryFactory queryFactory;
+
+    @BeforeEach // 테스트 실행전 실행됨
+    public void before() {
+        queryFactory = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        Member findMember = em.createQuery("select m from Member m where m.username =:username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQueryDsl() {
+//        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(member) // static import 추가
+                .from(member)
+                .where(member.username.eq("member1")) // PreparedStatement 로 쿼리가 나감, 파라미터 바인딩 처리
+                .fetchOne();
+
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+    }
+
+    @Test
+    public void search() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1").
+                        and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+        /*
+            member.username.eq("member1"); // username = 'member1'
+            member.username.ne("member1"); // username != 'member1'
+            member.username.eq("member1").not(); // username != 'member1'
+
+            member.username.isNotNull(); // 이름이 is not null
+
+            member.age.in(10, 20); // age in (10,20)
+            member.age.notIn(10, 20); // age not in (10, 20)
+            member.age.between(10, 30); // between 10, 30
+
+            member.age.goe(30); // age >= 30
+            member.age.gt(30); // age > 30
+            member.age.loe(30); // age <= 30
+            member.age.lt(30); // age < 30
+
+            member.username.like("member%"); // like 검색
+            member.username.contains("member"); // like '%member%' 검색
+            member.username.startsWith("member"); // like 'member%' 검색
+
+         */
+
+
+    }
+
+    @Test
+    public void searchAndParam() {
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"), member.age.eq(10)) // 메서드 체인 말고 그냥 쉼표로 적어도됨 (and) 인 경우
+                .fetchOne();
+
+
+        // 만약에 밑에 처럼
+        /*
+            .where(
+                member.username.eq("member1"),
+                member.age.eq(10),
+                null  <-- 이렇게 null 이 추가 되면 그냥 무시함. 이걸 이용해서 기가막힌 동적쿼리를 만들 수 있다
+         */
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void resultFetch() {
+//        List<Member> fetch = queryFactory
+//                .selectFrom(member)
+//                .fetch();
+//
+//        Member fetchOne = queryFactory
+//                .selectFrom(member)
+//                .fetchOne();
+//
+//        Member fetchFirst = queryFactory
+//                .selectFrom(member)
+//                .fetchFirst();// == limit(1).fetchOne()
+
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .offset(2)
+                .fetchResults(); //count 쿼리가 나감
+
+        System.out.println("results.getOffset() = " + results.getOffset());
+        List<Member> content = results.getResults();
+        for (Member member1 : content) {
+            System.out.println("member1 = " + member1);
+        }
+
+
+//        long total = queryFactory
+//                .selectFrom(member)
+//                .fetchCount();
+    }
+
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단, 2에서 회원 이름이 없으면 마지막에 출력(nulls last)
+     */
+    @Test
+    public void sort() {
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+
+        List<Member> findMembers = queryFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = findMembers.get(0);
+        Member member6 = findMembers.get(1);
+        Member memberNull = findMembers.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
+        for (Member findMember : findMembers) {
+            System.out.println("findMember = " + findMember);
+        }
+
+    }
+
+    @Test
+    public void paging1() {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void paging2() {
+        QueryResults<Member> queryResults = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetchResults();
+
+        System.out.println("----------------------------");
+        assertThat(queryResults.getTotal()).isEqualTo(4);
+        System.out.println("----------------------------");
+        assertThat(queryResults.getLimit()).isEqualTo(2);
+        assertThat(queryResults.getOffset()).isEqualTo(1);
+        assertThat(queryResults.getResults().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void aggregation() {
+        List<Tuple> result = queryFactory // Tuple을 사용하는 이유는 쿼리나가고 값의 타입이 여러개여서, 단일 타입이아닐때 사용. 나중에 Dto 로 뽑아오게 할거임
+                .select(
+                        member.count(),
+                        member.age.sum(),
+                        member.age.avg(),
+                        member.age.max(),
+                        member.age.min()
+                )
+                .from(member)
+                .fetch();
+
+        Tuple tuple = result.get(0);
+        assertThat(tuple.get(member.count())).isEqualTo(4);
+        assertThat(tuple.get(member.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(member.age.max())).isEqualTo(40);
+        assertThat(tuple.get(member.age.min())).isEqualTo(10);
+        assertThat(tuple.get(member.age.avg())).isEqualTo(25);
+
+    }
+
+    /**
+     * 팀의 이름과 각 팀의 평균 연령을 구해라
+     */
+    @Test
+    public void group() {
+        List<Tuple> result = queryFactory
+                .select(team.name, member.age.avg())
+                .from(member)
+                .join(member.team, team)
+                .groupBy(team.name)
+                .fetch();
+
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(team.name)).isEqualTo("teamA");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(15); // 10, 20
+
+        assertThat(teamB.get(team.name)).isEqualTo("teamB");
+        assertThat(teamA.get(member.age.avg())).isEqualTo(35); // 30, 40
+
+
+    }
+    
+}
